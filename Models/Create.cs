3d6c24518a8b203cec28ed1e3ba375b68f3a162b
szafranic <ryan.szafranic@gmail.com>
2022-10-27 16:55:37 -0400

    initial

---
+++ Models/Create.cs
@@ -0,0 +1,12 @@
+﻿namespace MyCoffeeShop.Models
+{
+	public class Create
+	{
+		public string Fname { get; set; }
+		public string Lname { get; set; }
+		public string Email { get; set; }
+		public string Pword { get; set; }
+		public string Color { get; set; }
+
+	}
+}
